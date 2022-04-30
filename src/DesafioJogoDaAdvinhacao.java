d4b7aedba8240f8539a6a5fac2d7b552876e303f
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-04-29 21:08:12 -0300

    Desafio em desenvolvimento

--- src/DesafioJogoDaAdvinhacao.java
+++ src/DesafioJogoDaAdvinhacao.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class DesafioJogoDaAdvinhacao {
     public static void main(String[] args) {
+    int numeroDigitado,qtdTentativas;
+        Scanner scan = new Scanner(System.in);
+        Random gerador = new Random();
+        numeroDigitado = gerador.nextInt(100) + 1;
+        System.out.println(numeroDigitado);
 
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Informe um número entre 0 e 100");
+        }
     }
 }
