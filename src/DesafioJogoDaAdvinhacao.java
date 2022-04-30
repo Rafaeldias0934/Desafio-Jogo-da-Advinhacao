a3cce35ca8d51280bf73a79573c9306dbb91e51e
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-04-29 22:30:10 -0300

    Desafio em desenvolvimento

--- src/DesafioJogoDaAdvinhacao.java
+++ src/DesafioJogoDaAdvinhacao.java
@@ -3,14 +3,25 @@ import java.util.Scanner;
 
 public class DesafioJogoDaAdvinhacao {
     public static void main(String[] args) {
-    int numeroDigitado,qtdTentativas;
+    int numeroAleatorio;
         Scanner scan = new Scanner(System.in);
         Random gerador = new Random();
-        numeroDigitado = gerador.nextInt(100) + 1;
-        System.out.println(numeroDigitado);
+        numeroAleatorio = gerador.nextInt(100) + 1;
 
         for (int i = 10; i >= 1; i--) {
-            System.out.println("Informe um número entre 0 e 100");
+            System.out.println("Informe um número entre 0 e 100.");
+            int numeroDigitado = scan.nextInt();
+            if (numeroDigitado <= 0 || numeroDigitado >= 100) {
+                System.out.println("O número digitado é inválido, please, digite um valor entre 0 e 100.");
+            } else if (numeroDigitado < numeroAleatorio) {
+                System.out.println("O número digitado é menor que o número aleatório.");
+            } else if (numeroDigitado > numeroAleatorio){
+                System.out.println("O número digitado é maior que o número aleatório.");
+            } else {
+                System.out.println("Acertoou!! O número aleatório é " + numeroAleatorio);
+                break;
+            }
+            System.out.println("Restam " + (i-1) + " tentativas.");
         }
     }
 }
